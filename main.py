8544a6ac55a952beb60c14a6ba088d234b910bb7
Georg <georgtsvetkof@gmail.com>
2024-12-02 17:51:38 +0300

    2

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
 from random import randint
-from PyQt6 import uic
 from PyQt6.QtCore import QPointF
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QMainWindow, QApplication
+from UI import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.setFixedSize(1000, 500)
         self.setWindowTitle("Mr beast")
         self.pushButton.resize(800, 200)
@@ -30,7 +30,8 @@ class MyWidget(QMainWindow):
 
     def draw_ellipse(self, qp):
         qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
-        qp.drawEllipse(QPointF(500, 300), 75, 75)
+        r = randint(20, 100)
+        qp.drawEllipse(QPointF(500, 300), r, r)
 
 
 def except_hook(cls, exception, traceback):
@@ -43,4 +44,3 @@ if __name__ == '__main__':
     ex.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
-
